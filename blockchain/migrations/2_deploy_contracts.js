24c634a1c7b99c6fd3fe06cb7d2afda186a9ed5d
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2024-08-25 04:59:33 +0700

    đa fix xong dang xuat, dang nhap. Tiep theo la tao lo hang va them anh vao ipfs

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
+// 2_deploy_contracts.js
 const SupplyChain = artifacts.require("SupplyChain");
 
-module.exports = function (deployer) {
-  deployer.deploy(SupplyChain);
+module.exports = function(deployer, network, accounts) {
+    const admin = accounts[0]; // Hoặc bất kỳ địa chỉ nào bạn muốn sử dụng làm admin
+    deployer.deploy(SupplyChain, admin);
 };
