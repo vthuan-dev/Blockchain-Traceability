b90616a553e1d4e04935f2faf8da33cd06021855
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2024-08-24 23:15:08 +0700

    move html file to blockchain/src/public folder

---
+++ blockchain/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path'); // Thêm module path để xử lý đường dẫn
+const app = express();
+const dangkyRoutes = require('./components/user/dangky');
+const dangnhapRoutes = require('./components/user/dangnhap'); // Import router đăng nhập
+
+// Sử dụng body-parser để phân tích cú pháp JSON từ yêu cầu
+app.use(bodyParser.json());
+
+
+// Cấu hình đường dẫn tĩnh để phục vụ các tệp tĩnh
+
+// Sử dụng router đăng ký
+app.use('/api', dangkyRoutes);
+
+// Sử dụng router đăng nhập
+app.use('/api', dangnhapRoutes);
+
+//test
+
+
+app.listen(3000, function() {
+    console.log('Server is running on port 3000');
+});
