865d9d91bda8616fb130b4561b2d907f793f6fb9
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2024-09-05 15:01:24 +0700

    1

--- blockchain/contracts/supplychain.sol
+++ blockchain/contracts/supplychain.sol
@@ -157,8 +157,3 @@ contract TraceabilityContract {
 
         return batches;
     }
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> Branchcualoc
--- blockchain/src/test.js
+++ blockchain/src/test.js
@@ -371,8 +371,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> Branchcualoc
 
