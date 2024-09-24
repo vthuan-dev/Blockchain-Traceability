4713f66ee2363543e492cdf769696c3600538169
LocNguyen-807 <locb2111807@student.ctu.edu.vn>
2024-09-24 13:05:56 +0700

    commit

--- blockchain/src/components/user/dangnhap.js
+++ blockchain/src/components/user/dangnhap.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const path = require('path');
-//const mysql = require('mysql2/promise');
 const { sendEmail } = require('./sendmail');
 
 const router = express.Router();
@@ -33,18 +32,11 @@ module.exports = function(db) {
                     return res.status(401).json({ message: 'Email hoặc mật khẩu không đúng' });
                 }
 
- // Giả sử có trường admin_id
-           //     req.session.isAdmin = true;
-   
-
-                // Tạo session cho admin
                 req.session.adminId = admin.id;
                 req.session.adminEmail = admin.admin_email;
                 req.session.adminName = admin.admin_name; 
-                req.session.province_id = admin.province_id; // Lưu province_id vào session
+                req.session.province_id = admin.province_id;
 
-
-                // Trả về thông tin admin
                 res.status(200).json({ 
                     message: 'Đăng nhập thành công', 
                     admin: {
@@ -52,7 +44,7 @@ module.exports = function(db) {
                         adminEmail: admin.admin_email,
                         adminName: admin.admin_name,
                         province_id: admin.province_id,
-                        roleId: 3 // Giả sử tất cả admin đều có roleId là 3
+                        roleId: 3
                     }
                 });
             } else {
@@ -76,23 +68,19 @@ module.exports = function(db) {
                     return res.status(403).json({ message: 'Tài khoản của bạn chưa xác thực, vui lòng xác thực Email trước khi đăng nhập.' });
                 }
 
-                if (isAdmin && user.role_id !== 3) { // Giả sử role_id 3 là Admin
+                if (isAdmin && user.role_id !== 3) {
                     return res.status(403).json({ message: 'Bạn không có quyền đăng nhập với tư cách Admin' });
                 }
 
-                // Thiết lập session cho user
                 req.session.userId = user.uid;
                 req.session.isAdmin = false;
                 req.session.name = user.name;
                 req.session.email = user.email;
                 req.session.roleId = user.role_id;
-                req.session.province_id = user.province_id; // Lưu province_id vào session
-                req.session.region_id = user.region_id; // Lưu region_id vào session
-                req.session.region = user.region; // Lưu region vào session
+                req.session.province_id = user.province_id;
+                req.session.region_id = user.region_id;
+                req.session.region = user.region;
 
-
-                
-                // Trả về thông tin người dùng và role_id
                 res.status(200).json({ 
                     message: 'Đăng nhập thành công', 
                     user: {
@@ -113,32 +101,28 @@ module.exports = function(db) {
         }
     });
 
-    // Thêm route để phục vụ trang quenmatkhau.html
     router.get('/quenmatkhau', (req, res) => {
         res.sendFile(path.join(__dirname, '../../public/account/quenmatkhau.html'));
     });
 
-    // Tạo biến toàn cục để lưu trữ mật khẩu mới
     let globalNewPassword = '';
 
-    // Thêm route để xử lý yêu cầu đặt lại mật khẩu
     router.post('/reset-passwd', async function(req, res) {
         const { email, newPassword, captcha } = req.body;
 
-    try {
-        const [users] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
-        const user = users[0];
+        try {
+            const [users] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
+            const user = users[0];
 
-        if(!user) {
-            return res.status(400).json({ message: 'Email không tồn tại' });
-        }
-    
-        if(!user.is_approved) {
-            return res.status(400).json({ message: 'Tài khoản chưa được phê duyệt' });
-        }
+            if(!user) {
+                return res.status(400).json({ message: 'Email không tồn tại' });
+            }
+        
+            if(!user.is_approved) {
+                return res.status(400).json({ message: 'Tài khoản chưa được phê duyệt' });
+            }
 
             globalNewPassword = newPassword; 
-            // Lấy token từ cơ sở dữ liệu dựa trên email
             const [tokenResult] = await db.query('SELECT verificationToken FROM users WHERE email = ?', [email]);
             const token = tokenResult[0].verificationToken;
             
@@ -146,7 +130,6 @@ module.exports = function(db) {
                 return res.status(500).json({ message: 'Lỗi khi lấy token' });
             }
 
-            // Gửi email thông báo đặt lại mật khẩu
             const resetLink = `http://localhost:3000/api/reset-password/${token}`;
             const templatePath = path.join(__dirname, '../../public/account/xacthuc.html');
             await sendEmail(email, user.name, resetLink, 'Xác thực đặt lại mật khẩu', templatePath);
@@ -161,7 +144,6 @@ module.exports = function(db) {
     router.get('/reset-password/:token', async function(req, res) {
         const { token } = req.params;
         try {
-            // Kiểm tra token trong cơ sở dữ liệu
             const [users] = await db.query('SELECT * FROM users WHERE verificationToken = ?', [token]);
             const user = users[0];
 
@@ -169,13 +151,11 @@ module.exports = function(db) {
                 return res.status(400).send('Token đặt lại mật khẩu không hợp lệ hoặc đã hết hạn.');
             }
 
-            // Hash mật khẩu mới
-            const hashedPassword = await bcrypt.hash(globalNewPassword, 10); // Sử dụng chuỗi thay vì mảng
+            const hashedPassword = await bcrypt.hash(globalNewPassword, 10);
 
-            // Cập nhật mật khẩu mới trong cơ sở dữ liệu
             await db.query('UPDATE users SET passwd = ? WHERE verificationToken = ?', [hashedPassword, token]);
 
-            res.redirect('/dangnhap.html'); // Chuyển hướng đến trang đăng nhập
+            res.redirect('/dangnhap.html');
         } catch (error) {
             console.error('Lỗi khi đặt lại mật khẩu:', error);
             res.status(500).send('Đã xảy ra lỗi khi đặt lại mật khẩu.');
