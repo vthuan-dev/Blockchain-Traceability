d2088c4f0bca25a3cafc1581907fccd2ef9f6e9c
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2024-10-02 02:17:36 +0700

    adding user take part in batchlogs

--- blockchain/contracts/ActivityLog.sol
+++ blockchain/contracts/ActivityLog.sol
@@ -2,99 +2,97 @@
 pragma solidity ^0.8.0;
 
 contract ActivityLog {
-    struct ActivityLogEntry {
-        uint256 timestamp;
-        uint256 uid;
-        string activityName;
-        string description;
-        bool isSystemGenerated;
-        string[] imageUrls;
-        uint256[] relatedProductIds;
-    }
+ struct ActivityLogEntry {
+    uint256 batchId;
+    uint256 participantId;
+    uint256 timestamp;
+    string activityName;
+    string description;
+    bool isSystemActivity;
+    string[] imageUrls;
+    uint256[] relatedProductIds;
+}
 
     mapping(uint256 => ActivityLogEntry[]) private _activityLogs;
 
-    event ActivityLogAdded(
-        uint256 indexed uid,
-        string activityName,
-        string description,
-        bool isSystemGenerated,
-        string[] imageUrls,
-        uint256[] relatedProductIds
-    );
-
+  event ActivityLogAdded(
+    uint256 indexed batchId,
+    uint256 indexed participantId,
+    string activityName,
+    string description,
+    bool isSystemGenerated,
+    string[] imageUrls,
+    uint256[] relatedProductIds
+);
     function addActivityLog(
-
-        uint256 _uid,
+        uint256 _batchId,
+        uint256 _participantId,
         string memory _activityName,
         string memory _description,
-        bool _isSystemGenerated,
+        bool _isSystemActivity,  // Đổi tên tham số này
         string[] memory _imageUrls,
         uint256[] memory _relatedProductIds
     ) public {
         ActivityLogEntry memory newLog = ActivityLogEntry({
+            batchId: _batchId,
+            participantId: _participantId,
             timestamp: block.timestamp,
-            uid: _uid,
             activityName: _activityName,
             description: _description,
-            isSystemGenerated: _isSystemGenerated,
+            isSystemActivity: _isSystemActivity,  // Và ở đây
             imageUrls: _imageUrls,
             relatedProductIds: _relatedProductIds
         });
 
-        _activityLogs[_uid].push(newLog);
+        _activityLogs[_batchId].push(newLog);
 
-        emit ActivityLogAdded(_uid, _activityName, _description, _isSystemGenerated, _imageUrls, _relatedProductIds);
+        emit ActivityLogAdded(_batchId, _participantId, _activityName, _description, _isSystemActivity, _imageUrls, _relatedProductIds);
     }
     function getActivityLogs(uint256 _uid) public view returns (ActivityLogEntry[] memory) {
         return _activityLogs[_uid];
     }
 
-    function getActivityLogCount(uint256 _uid) public view returns (uint256) {
-        return _activityLogs[_uid].length;
+
+function getProducerActivityLogs(uint256 _uid) public view returns (ActivityLogEntry[] memory) {
+    ActivityLogEntry[] memory allLogs = _activityLogs[_uid];
+    uint256 count = 0;
+    for (uint256 i = 0; i < allLogs.length; i++) {
+        if (!allLogs[i].isSystemActivity) {  // Sửa ở đây
+            count++;
+        }
     }
 
-    function getProducerActivityLogs(uint256 _uid) public view returns (ActivityLogEntry[] memory) {
-        ActivityLogEntry[] memory allLogs = _activityLogs[_uid];
-        uint256 count = 0;
-        for (uint256 i = 0; i < allLogs.length; i++) {
-            if (!allLogs[i].isSystemGenerated) {
-                count++;
-            }
+    ActivityLogEntry[] memory producerLogs = new ActivityLogEntry[](count);
+    uint256 index = 0;
+    for (uint256 i = 0; i < allLogs.length; i++) {
+        if (!allLogs[i].isSystemActivity) {  // Và ở đây
+            producerLogs[index] = allLogs[i];
+            index++;
         }
-
-        ActivityLogEntry[] memory producerLogs = new ActivityLogEntry[](count);
-        uint256 index = 0;
-        for (uint256 i = 0; i < allLogs.length; i++) {
-            if (!allLogs[i].isSystemGenerated) {
-                producerLogs[index] = allLogs[i];
-                index++;
-            }
-        }
-
-        return producerLogs;
     }
 
-    function getSystemActivityLogs(uint256 _uid) public view returns (ActivityLogEntry[] memory) {
-        ActivityLogEntry[] memory allLogs = _activityLogs[_uid];
-        uint256 count = 0;
-        for (uint256 i = 0; i < allLogs.length; i++) {
-            if (allLogs[i].isSystemGenerated) {
-                count++;
-            }
+    return producerLogs;
+}
+function getSystemActivityLogs(uint256 _uid) public view returns (ActivityLogEntry[] memory) {
+    ActivityLogEntry[] memory allLogs = _activityLogs[_uid];
+    uint256 count = 0;
+    for (uint256 i = 0; i < allLogs.length; i++) {
+        if (allLogs[i].isSystemActivity) {  // Sửa ở đây
+            count++;
         }
-
-        ActivityLogEntry[] memory systemLogs = new ActivityLogEntry[](count);
-        uint256 index = 0;
-        for (uint256 i = 0; i < allLogs.length; i++) {
-            if (allLogs[i].isSystemGenerated) {
-                systemLogs[index] = allLogs[i];
-                index++;
-            }
-        }
-
-        return systemLogs;
     }
+
+    ActivityLogEntry[] memory systemLogs = new ActivityLogEntry[](count);
+    uint256 index = 0;
+    for (uint256 i = 0; i < allLogs.length; i++) {
+        if (allLogs[i].isSystemActivity) {  // Và ở đây
+            systemLogs[index] = allLogs[i];
+            index++;
+        }
+    }
+
+    return systemLogs;
+}
 
     function getSystemActivityLogsBySSCC(string memory _sscc) public view returns (ActivityLogEntry[] memory) {
         uint256 batchId = uint256(keccak256(abi.encodePacked(_sscc)));
