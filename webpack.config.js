24d6e7eac0a56b055715854f92ea991ce3707542
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2025-02-25 18:09:11 +0700

    Fix webpack configuration for Heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./blockchain/src/public/chatbox/chat.bundle.js", // Điểm vào ứng dụng React từ thư mục src
+  entry: "./src/public/chatbox/index.js", // Điều chỉnh đường dẫn này theo cấu trúc thư mục thực tế của bạn
   output: {
     path: path.resolve(__dirname, "./blockchain/src/public/chatbox"), // Nơi chứa file sau khi bundling
     filename: "chat.bundle.js", // Tên file sau khi bundling
@@ -25,7 +25,7 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"], // Giúp webpack hiểu file .js và .jsx
   },
-  mode: "development", // Chế độ development
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development', // Chế độ development
   devServer: {
     static: {
       directory: path.join(__dirname, "./blockchain/src/public"),
